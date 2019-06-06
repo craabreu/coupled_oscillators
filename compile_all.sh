d0fb12ff369e5f1af134e57db5867ef6134a62fa
Charlles Abreu <abreu@eq.ufrj.br>
2019-06-05 23:15:01 -0400

    New results

--- compile_all.sh
+++ compile_all.sh
@@ -2,12 +2,13 @@
 
 methods=$(grep -e '^\s*subroutine' harmonic.f90 | sed -e 's/(.*)//g' -e 's/  subroutine //g')
 echo $methods
-for n in 1 2 4; do
-  for nrespa in `seq 5 5 50`
-    for method in $methods; do
+for method in $methods; do
+  for n in 1 2 4; do
+    for nrespa in `seq 5 5 50`; do
       make METHOD=$method NN=$n NRESPA=$nrespa clean
       make METHOD=$method NN=$n NRESPA=$nrespa
-      mkdir -p $method
-      mv harmonic-${method}* ${method}/
     done
+  done
+  mkdir -p $method
+  mv harmonic-${method}* ${method}/
 done
