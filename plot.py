381976fdbcb9dfa8ad2b3d36cc98b74ae491369c
Charlles Abreu <abreu@eq.ufrj.br>
2019-06-10 13:30:05 -0400

    Consolitation of results for L=5

--- plot.py
+++ plot.py
@@ -1,10 +1,12 @@
 import os
+import glob
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 methods = [
-    'BAOAB',
+    # 'BAOAB',
+    # 'memory_BAOAB',
     'middle_IsoK_NHC',
     'middle_NHC',
     'middle_NHL',
@@ -12,11 +14,17 @@ methods = [
     'middle_SubK_NHC',
     'xi_respa_NHC',
 ]
-first = 10
-last = 90
-step = 10
 
-L = 10        # Ratio between slow and fast periods of oscillation
+# L = 5         # Ratio between slow and fast periods of oscillation
+# first = 5
+# last = 100
+# step = 5
+
+L = 10         # Ratio between slow and fast periods of oscillation
+first = 5
+last = 100
+step = 5
+
 m = 1         # Body mass
 kT = 1        # Boltzmann constant times temperature
 
@@ -30,19 +38,20 @@ def potential(sigma):
 Z2 = np.zeros((2,2))
 I2 = np.identity(2)
 sigma_exact = kT*np.block([[np.linalg.inv(K), Z2], [Z2, m*I2]])
-U_exact = potential(sigma_exact)
 
 frames = {}
 for n in [1, 2, 4]:
     data = {'nrespa': range(first, last+1, step)}
     for method in methods:
         values = []
-        for nrespa in range(first, last+1, step):
-            file = os.path.join(method, f'harmonic-{method}-n{n}-respa{nrespa}.sigma')
-            sigma = np.loadtxt(file)
-            U = potential(sigma)
-            values.append(U-U_exact)
-        data[method] = values
+        files = glob.glob(f'L10/harmonic-{method}-n{n}*.sigma')
+        if files:
+            for nrespa in range(first, last+1, step):
+                file = os.path.join(f'L{L}', f'harmonic-{method}-n{n}-respa{nrespa}.sigma')
+                sigma = np.loadtxt(file)
+                U = potential(sigma)
+                values.append(U - 1.0)
+            data[method] = values
     frames[f'n={n}'] = pd.DataFrame(data)
 
 for title, frame in frames.items():
